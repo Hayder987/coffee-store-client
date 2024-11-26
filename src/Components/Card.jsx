2384429504f70554185251e206a054e731a96869
hayder <hayderbd4290@gmail.com>
2024-11-26 20:12:32 +0600

    delete function created

---
+++ src/Components/Card.jsx
@@ -0,0 +1,72 @@
+import { FaEye } from "react-icons/fa";
+import { MdDelete, MdEdit } from "react-icons/md";
+import Swal from "sweetalert2";
+
+
+const Card = ({coffee, allData, setAllData}) => {
+   
+  const {_id, name, chef, supplier, taste, category, details, imgPath} = coffee || {}
+
+  const deleteHandler = (_id)=>{
+
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+
+            fetch(`http://localhost:4000/coffees/${_id}`,{
+                method: "DELETE"
+            })
+            .then(res=> res.json())
+            .then(item=>{
+                const remaining = allData.filter(data=> data._id !== _id)
+                setAllData(remaining)
+                  if(item.deletedCount>0){
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                      }); 
+                  } 
+            });
+            
+        }
+      });
+  }
+
+    return (
+        <div className="bg-[#F5F4F1] rounded-xl flex justify-between items-center py-4 px-4 lg:px-12">
+            <div className="">
+               <img src={imgPath} alt="" />
+            </div>
+            <div className="flex flex-col gap-3">
+              <h3 className="">
+                <span className="font-font1 font-bold">Name: </span>
+                <span className="">{name}</span>
+              </h3>
+              <h3 className="">
+                <span className="font-font1 font-bold">Chef: </span>
+                <span className="">{chef}</span>
+              </h3>
+              <h3 className="">
+                <span className="font-font1 font-bold">Category: </span>
+                <span className="">{category}</span>
+              </h3>
+            </div>
+            <div className="flex flex-col gap-5">
+                <button className="bg-[#D2B48C] p-3 text-xl text-white rounded-lg"><FaEye /></button>
+                <button className="bg-[#3C393B] p-3 text-xl text-white rounded-lg"><MdEdit /></button>
+                <button onClick={()=>deleteHandler(_id)} className="bg-[#EA4744] p-3 text-xl text-white rounded-lg"><MdDelete /></button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import Banner from "../Components/Banner";
 import BannerCard from "../Components/BannerCard";
+import { useLoaderData } from "react-router";
+import Card from "../Components/Card";
 
 
 const Home = () => {
+  const loaderData = useLoaderData()
+  const [allData, setAllData] = useState(loaderData)
+
+
     return (
         <div>
           <Banner></Banner>
           <BannerCard></BannerCard>
           <h1 className="font-font2 text-center font-semibold text-5xl text-[#331A15] mb-12">Our Popular Products</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-20">
+            {
+              allData.map(coffee => <Card key={coffee._id}
+                 coffee={coffee} 
+                 allData={allData}
+                 setAllData={setAllData}
+                 ></Card>)
+            }
+          </div>
         </div>
     );
 };
